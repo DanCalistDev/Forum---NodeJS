03f7283d9088ef75708423b8a603d4771eee3a03
Dancalist <dancalist.dev@gmail.com>
2024-03-04 13:10:51 -0300

    feat: :sparkles: implement findBySlug in memory question repository

--- test/repositories/in-memory-question-repository.ts
+++ test/repositories/in-memory-question-repository.ts
@@ -2,8 +2,18 @@ import { QuestionRepository } from "@/domain/forum/application/repositories/ques
 import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionRepository implements QuestionRepository {
-
     public items: Question[] = []
+
+    async findBySlug(slug: string){
+        const question = this.items.find((item) => item.slug.value === slug);
+ 
+ 
+        if(!question) {
+          return null
+        }
+ 
+        return question
+     }
 
     async create(question: Question){
         this.items.push(question)
