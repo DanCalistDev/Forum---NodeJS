247b25d103633e63cc70c7fc857911d8513d3d80
Dancalist <dancalist.dev@gmail.com>
2024-03-04 00:56:38 -0300

    refactor: :hammer: add inmemory answers

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,18 +1,32 @@
-import { AnswerQuestionUseCase } from './answer-question'
-import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from './../../enterprise/entities/answer';
+import { InMemoryAnswerRepository } from 'test/repositories/in-memory-answers-repository';
+import { InMemoryQuestionRepository } from './../../../../../test/repositories/in-memory-question-repository';
+import { CreateQuestionUseCase } from './create-question'
+import { AnswerQuestionUseCase } from './answer-question';
 
-const fakeAnswerRepository: AnswersRepository = {
-  create: async () => {},
-}
 
-test('create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+let inMemoryAnswerRepository: InMemoryAnswerRepository
+let sut: AnswerQuestionUseCase
 
-  const answer = await answerQuestion.execute({
-    questionId: '1',
-    instructorId: '1',
-    content: 'Nova resposta',
-  })
 
-  expect(answer.content).toEqual('Nova resposta')
+describe('Create Answers', () => {
+
+    beforeEach(() => {
+        inMemoryAnswerRepository = new InMemoryAnswerRepository()
+        sut = new AnswerQuestionUseCase(inMemoryAnswerRepository)
+    })
+
+
+    it('should be able to create a question', async () => {    
+        const { answer } =  await sut.execute({
+          questionId: '1',
+          instructorId: '1',
+          content: 'Conteúdo da resposta',
+        })
+      
+        expect(answer.id).toBeTruthy()
+        expect(inMemoryAnswerRepository.items[0].id).toEqual(answer.id)
+      })
 })
+
+
