2c16c526dd5ad3d224e294513bf9e621d2968f2b
Dancalist <dancalist.dev@gmail.com>
2024-03-04 15:20:24 -0300

    feat: :sparkles: implementing findById and delete in in memory question

--- test/repositories/in-memory-question-repository.ts
+++ test/repositories/in-memory-question-repository.ts
@@ -2,8 +2,19 @@ import { QuestionRepository } from "@/domain/forum/application/repositories/ques
 import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionRepository implements QuestionRepository {
+  
     public items: Question[] = []
 
+    async findById(id: string) {
+        const question =  this.items.find(item => item.id.toString() === id)
+
+        if(!question) {
+            return null
+        }
+        return question
+    }
+
+   
     async findBySlug(slug: string){
         const question = this.items.find((item) => item.slug.value === slug);
  
@@ -18,4 +29,11 @@ export class InMemoryQuestionRepository implements QuestionRepository {
     async create(question: Question){
         this.items.push(question)
     }
+
+    async delete(question: Question) {
+       const itemIndex = this.items.findIndex(item => item.id === question.id)
+
+       this.items.splice(itemIndex, 1)
+    }
+
 }
