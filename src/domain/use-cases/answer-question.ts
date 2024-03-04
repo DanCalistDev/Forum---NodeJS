fce927f3e01817c24107ebbd8a5ad7b4a005f4ee
Dancalist <dancalist.dev@gmail.com>
2024-03-03 22:46:27 -0300

    feat: :sparkles: initial use case - answer-question

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,28 @@
+import { AnswersRepository } from './../repositories/answers-repository';
+import { Answer } from "../entities/answer";
+import { UniqueEntityID } from '../../core/entities/unique-entity-id';
+
+interface AnswerQuestionUseCaseRequest {
+    instructorId: string;
+    questionId: string;
+    content: string;
+}
+
+
+export class AnswerQuestionUseCase {
+
+    constructor(private AnswersRepository: AnswersRepository) {}
+
+    async execute({instructorId, questionId, content}: AnswerQuestionUseCaseRequest) {
+        const answer = Answer.create({
+            content,
+            authorId: new UniqueEntityID(instructorId),
+            questionId: new UniqueEntityID(questionId),
+        })
+
+        await this.AnswersRepository.create(answer)
+
+        return answer
+
+    }
+}
