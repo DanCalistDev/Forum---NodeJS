c6ec74696f1b7c36c4cccbf33e0fef5f12d05361
Dancalist <dancalist.dev@gmail.com>
2024-03-03 23:44:05 -0300

    feat: :sparkles:  initial EsLint

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,24 +1,18 @@
-import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
-import { Answer } from '../entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 
-
 const fakeAnswerRepository: AnswersRepository = {
-    create: async (answer: Answer) => {
-        return
-    }
+  create: async () => {},
 }
 
 test('create an answer', async () => {
-    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
-    const answer = await answerQuestion.execute({
-        questionId: '1',
-        instructorId: '1',
-        content: 'Nova resposta',
-    })
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova resposta',
+  })
 
-
-    expect(answer.content).toEqual('Nova resposta')
+  expect(answer.content).toEqual('Nova resposta')
 })
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,28 +1,29 @@
-import { AnswersRepository } from './../repositories/answers-repository';
-import { Answer } from "../entities/answer";
-import { UniqueEntityID } from '../../core/entities/unique-entity-id';
+import { AnswersRepository } from './../repositories/answers-repository'
+import { Answer } from '../entities/answer'
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface AnswerQuestionUseCaseRequest {
-    instructorId: string;
-    questionId: string;
-    content: string;
+  instructorId: string
+  questionId: string
+  content: string
 }
 
-
 export class AnswerQuestionUseCase {
+  constructor(private AnswersRepository: AnswersRepository) {}
 
-    constructor(private AnswersRepository: AnswersRepository) {}
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    })
 
-    async execute({instructorId, questionId, content}: AnswerQuestionUseCaseRequest) {
-        const answer = Answer.create({
-            content,
-            authorId: new UniqueEntityID(instructorId),
-            questionId: new UniqueEntityID(questionId),
-        })
+    await this.AnswersRepository.create(answer)
 
-        await this.AnswersRepository.create(answer)
-
-        return answer
-
-    }
+    return answer
+  }
 }
