b961eba48bde1dd9d1601fd8e59fe11f89e8897a
Dancalist <dancalist.dev@gmail.com>
2024-03-04 22:11:42 -0300

    feat: :sparkles: create use case Fetch recent questions

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -0,0 +1,28 @@
+import { Question } from "../../enterprise/entities/question";
+import { QuestionRepository } from "../repositories/questions-repository";
+
+interface FetchRecentQuestionUseCaseRequest {
+  page: number
+
+}
+
+interface FetchRecentQuestionUseCaseResponse {
+    questions: Question[]
+  }
+
+export class FetchRecentQuestionUseCase {
+  constructor(private questionRepository: QuestionRepository) {}
+
+  async execute({
+    page,
+ 
+  }: FetchRecentQuestionUseCaseRequest): Promise<FetchRecentQuestionUseCaseResponse> {
+    const questions = await this.questionRepository.findManyRecent({page})
+
+   
+    return {
+      questions,
+    }
+
+  }
+}
